7e581160551ff12cc98e44b9139f9f22b86d96dc
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-25 21:03:11 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
 
   def self.current_user
-    User.find(session[:id])
+    User.find(session[:user_id])
   end
 
   def self.is_logged_in?
-    session[:id] ? true : false
+    session[:user_id] ? true : false
   end
 end
